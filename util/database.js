983161a379f3210614aac97c83746ecfa6b991da
kuron3ko89 <ian.chin@arq66820.lan>
2022-01-20 14:47:42 +1100

    tweaked db connection settings further v6-test

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // require('dotenv').config();
 
- const sequelize = new Sequelize('mysql', 'admin', 'Cybermew1!', {
+ const sequelize = new Sequelize('bank', 'admin', 'Cybermew1!', {
     host: 'aa1bx8edifnzb9x.cqwk4h8lorda.us-east-1.rds.amazonaws.com',
     port: 3306,
     logging: console.log,
