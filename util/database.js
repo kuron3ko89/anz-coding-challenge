5be0d157b3e8b1bdd7d8560c158b310f6ed55ff3
kuron3ko89 <ian.chin@arq66820.lan>
2022-01-20 14:02:21 +1100

    tweaked db connection settings further v4

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // require('dotenv').config();
 
- const sequelize = new Sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+ const sequelize = new Sequelize('mysql', process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT,
     logging: console.log,
